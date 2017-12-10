d9db6140d850e7267b8425a151fc3e788814d329
erik djamgarian <djamgarian0702@gmail.com>
2017-12-10 12:48:00 +0100

    basically done

---
+++ CodeReview3.js
@@ -0,0 +1,8 @@
+function calculateinsurance(){
+	var name = "John Doe";
+	var age = 20;
+	var country ="Austria";
+	var horsepower = 150;
+	var insurance = Math.round((horsepower*100)/(age+150));
+	document.getElementById("result").innerHTML = "Your insurance quota is " + insurance + "$";
+}
